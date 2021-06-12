68497d0b1fd1204c36d33ed3653e853d7469e71a
Aleksand Zuk <Aleksandrzuk30013010@gmail.com>
2021-06-12 22:40:15 +0300

    first commit

---
+++ script/script.js
@@ -0,0 +1,70 @@
+'use strict';
+// получаем переменные
+const todoControl = document.querySelector('.todo-control'),
+    headerInput = document.querySelector('.header-input'),
+    todoList = document.querySelector('.todo-list'),
+    todoCompleted = document.querySelector('.todo-completed');
+    
+  let todoData = [];
+// функция добавления дел  
+  const render = function() {
+      todoList.textContent = '';
+      todoCompleted.textContent = '';
+// добавление поля      
+      todoData.forEach(function(item){
+        const li = document.createElement('li');
+        li.classList.add('todo-item');
+
+        li.innerHTML = '<span class="text-todo">' + item.value + '</span>' +
+        '<div class="todo-buttons">' +
+            '<button class="todo-remove"></button>' +
+            '<button class="todo-complete"></button>' +
+        '</div>';
+
+      if(item.completed) {
+        todoCompleted.append(li);
+      } else {
+        todoList.append(li);
+      }
+// галочка выбора      
+      const btnTodoCompleted = li.querySelector('.todo-complete');
+          btnTodoCompleted.addEventListener('click', function(){
+              item.completed = !item.completed;
+            render();
+      })
+// кнопака корзины
+      const btnTodoRemove = li.querySelector('.todo-remove');
+          btnTodoRemove.addEventListener('click', function(){
+            li.querySelector('.text-todo').remove();
+            li.classList.remove('todo-item');
+        })
+       
+      });
+  };
+// кнопка добавления
+  todoControl.addEventListener('submit', function(event){
+    event.preventDefault();
+// ввод данных в поле
+    let getHeaderInput = function() {
+      if (headerInput.value === '') {
+          return;
+      } else {
+        const newTodo = {
+          value: headerInput.value, 
+          completed: false,
+        }; 
+      todoData.push(newTodo);
+// загрузка в localStorage
+      localStorage.ar = JSON.stringify(todoData);
+// отчистка поля ввода
+      headerInput.value = '';
+      
+      }
+    }; getHeaderInput();
+
+    render();
+  })
+  
+render();
+// вывод из localStorage
+todoData = JSON.parse(localStorage.ar);
